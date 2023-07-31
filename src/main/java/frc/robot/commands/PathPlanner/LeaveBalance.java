42a18181c7a068ef0c9a7b5e1302b4dff5153e5d
flareware95 <flareware95@gmail.com>
2023-07-31 15:51:53 -0400

    tuning balance

--- src/main/java/frc/robot/commands/PathPlanner/LeaveBalance.java
+++ src/main/java/frc/robot/commands/PathPlanner/LeaveBalance.java
@@ -6,6 +6,7 @@ import com.pathplanner.lib.PathPlannerTrajectory;
 import com.pathplanner.lib.commands.FollowPathWithEvents;
 import edu.wpi.first.wpilibj2.command.*;
 import frc.robot.Constants;
+import frc.robot.commands.driveCommands.DriveToAngle;
 import frc.robot.commands.driveCommands.DriveToLevel;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.PigeonSubsystem;
@@ -14,26 +15,28 @@ public class LeaveBalance extends SequentialCommandGroup {
 
     public LeaveBalance(DriveSubsystem driveSubsystem){
 
-        PathPlannerTrajectory m_conePath = PathPlanner.loadPath("Cone Score 4",
-                new PathConstraints(2, 3));
+        // PathPlannerTrajectory m_conePath = PathPlanner.loadPath("Cone Score 4",
+        //         new PathConstraints(2, 3));
+
         PathPlannerTrajectory m_pickupPath;
 
         m_pickupPath = PathPlanner.loadPath("Leave 4 Level Shoot",
                 new PathConstraints(1.4,  2.0));
 
+        //                PathPlannerTrajectory m_leaveLongLevel = PathPlanner.loadPath("Leave 5 Level",
+        //    new PathConstraints(1.4, 3));
+
+
         driveSubsystem.setTrajPID(
                 Constants.PathConstants.kpXdefault, Constants.PathConstants.kiXdefault, Constants.PathConstants.kdXdefault,
                 Constants.PathConstants.kpYdefault + 3, Constants.PathConstants.kiYdefault, Constants.PathConstants.kdYdefault,
-                1.0, Constants.PathConstants.kiRdefault, 0.2);
+                1.0, Constants.PathConstants.kiRdefault, 0.0);
 
         addCommands(
-//                new InstantCommand(() -> intakeSubsystem.intakeOn(IntakeConstants.kConePickUp), intakeSubsystem),
-//                new ArmToPosition(armSubsystem, armPositions.LVLTRE, true).withTimeout(1.20),
-                new FollowPathWithEvents(
-                        driveSubsystem.followTrajectoryCommand(m_conePath, true),
-                        m_conePath.getMarkers(),
-                        Constants.AutoConstants.AUTO_EVENT_MAP),
-//                new RunCommand(() -> intakeSubsystem.intakeOn(IntakeConstants.kConeEject), intakeSubsystem).withTimeout(.25),
+                // new FollowPathWithEvents(
+                //         driveSubsystem.followTrajectoryCommand(m_conePath, true),
+                //         m_conePath.getMarkers(),
+                //         Constants.AutoConstants.AUTO_EVENT_MAP),
 
                 new FollowPathWithEvents(
                         driveSubsystem.followTrajectoryCommand(m_pickupPath, false, true),
@@ -41,7 +44,7 @@ public class LeaveBalance extends SequentialCommandGroup {
                         Constants.AutoConstants.AUTO_EVENT_MAP),
 
                 new ParallelCommandGroup(
-                        new DriveToLevel(driveSubsystem),
+                        new DriveToAngle(driveSubsystem),
 //                                .alongWith(new RunCommand(() -> LED.cycle())),
                         new SequentialCommandGroup(
                                 new WaitCommand(0.5)
